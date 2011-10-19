796487a849a36aef0860529ad34e04ba30fbb252
webmasterpf-DG <webmaster@provence-formation.com>
2011-10-19 17:21:05 +0200

    Fin DEV page liste
    Signed-off-by: webmasterpf-DG <webmaster@provence-formation.com>

--- includes/inc_liste_vdl.php
+++ includes/inc_liste_vdl.php
@@ -1,7 +1,7 @@
 <?php
 /* 
  * Inclus la vue des actualités globales
- * dans un bloc pour liste des actualités générales
+ * dans un bloc pour liste des archives
  * IMPORTANT Style affichage  : grille !!
  */
 
@@ -10,7 +10,7 @@
       
     <?php
 
-$viewname = 'Liste_lycee_entreprise';
+$viewname = 'Liste_vdl';
 $view = views_get_view ($viewname);
 $view->set_display('block_1');
 
@@ -21,7 +21,7 @@ $view->execute();
 
 if ($view->result) {
   // S'il y a un resultat on récupère le titre (ajoute tag h3, et le contenu)
- $output = '<div id="bloc_liste_lycee-entreprise"><h3>'.$view->get_title().'</h3>' . $view->render().'</div>';
+ $output = '<div id="bloc_liste_vdl"><h3>'.$view->get_title().'</h3>' . $view->render().'</div>';
  
 }
 
