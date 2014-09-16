65339c1fef53e0736f5ba5a98ca4ed4ee77d31ea
webmasterpf-DG <webmaster@provence-formation.com>
2014-09-16 17:17:12 +0200

    creation d'un tpl pour page-page_liste.tpl.php
    Signed-off-by: webmasterpf-DG <webmaster@provence-formation.com>

---
+++ node/node-page_liste.tpl.php
@@ -0,0 +1,79 @@
+<?php
+/* Ce template permet la création d'un layout multicolonne pour le spages de base, en permettant la disposition facile
+ * des champs CCK custom, si nécessaires pour une page de base.
+*/?>
+<!--______________NODE TPL POUR node-page_vdl.tpl CUSTOM________________ -->
+<div class="node <?php print $classes; ?>" id="node-<?php print $node->nid; ?>">
+    <div class="node-inner">
+        <!--______________COLONNE 1________________ -->
+        <?php /* choix du layout selon nombre de colonne
+         * .col1_layout_200_590_200{} .col1_layout_330_all{} .col1_layout_18_56_25{}
+         * .col2_layout_200_590_200{} .col2_layout_330_all{} .col2_layout_18_56_25{}
+         * .col3_layout_200_590_200{} .col3_layout_330_all{} .col3_layout_18_56_25{}
+         */?>
+      <div id="col_G1" class="page_vdl_content_col_G1">
+           
+           <?php 
+              $theme_path = drupal_get_path('theme', 'cyrano_cg');
+              include ($theme_path.'/includes/inc_vue_grawitz_actus.php');
+              ?>
+             <?php
+              $theme_path = drupal_get_path('theme', 'cyrano_cg');
+              include ($theme_path.'/includes/inc_region_col_G1.php');
+              ?>
+        </div>
+        <!--______________COLONNE 2________________ -->
+         <!-- <pre> <?php //print_r($node); ?> </pre>-->   <!-- listage des variables du $content -->
+         <div id="col_G2" class="page_vdl_content_col_G2">
+
+              <?php if ($title): /*copier le titre dans la colonne desirée*/?>
+            <h1 class="titre_page_vdl"><?php print $title; ?></h1>
+            <?php endif; ?>
+            
+            <?php print $picture; ?>
+
+            <?php if ($submitted): ?>
+            <span class="submitted"><?php print $submitted; ?></span>
+            <?php endif; ?>
+
+            <div class="content">
+                <?php   print $node->content['body']['#value'];/*déplacer le contenu dans la colonne désirée*/ ?>
+                
+              <?php /*inclusion d'une vue via un champ viewreference*/
+              if ($node->field_choix_liste[0]['view']): ?>
+            <div id="bloc_liste_vdl">
+                    <?php  print $node->field_choix_liste[0]['view']  ?>
+            </div>
+            <?php endif;?>
+
+                    <?php
+              $theme_path = drupal_get_path('theme', 'cyrano_cg');
+              include ($theme_path.'/includes/inc_region_col_G2.php');
+              ?>
+
+            </div>
+
+        </div>
+
+        <!--______________COLONNE 3________________ -->
+      <div id="col_G3" class="page_vdl_content_col_G3">
+         
+
+
+
+    <?php
+              $theme_path = drupal_get_path('theme', 'cyrano_cg');
+              include ($theme_path.'/includes/inc_region_col_G3.php');
+              ?>
+        </div>
+
+        <?php if ($terms): ?>
+        <div class="taxonomy"><?php //print $terms; ?></div>
+        <?php endif;?>
+
+        <?php if ($links): ?>
+        <div class="links"> <?php //print $links; ?></div>
+        <?php endif; ?>
+
+    </div> <!-- /node-inner -->
+</div> <!-- /node-->
